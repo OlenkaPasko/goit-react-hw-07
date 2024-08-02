1dcd064faf0587d303c7193de9d67f0428d9d9b8
OlenkaPasko <113981072+OlenkaPasko@users.noreply.github.com>
2024-08-02 09:07:52 +0300

    createAsyncThunk

--- src/redux/contactsOps.js
+++ src/redux/contactsOps.js
@@ -3,4 +3,20 @@ import axios from "axios";
 
 axios.defaults.baseURL = "https://66ac6497f009b9d5c731e491.mockapi.io";
 
-export const fetchContacts = createAsyncThunk("tasks", async ()=>{})
+export const fetchContacts = createAsyncThunk("contacts/fetchAll", async () => {
+    try {
+        const response = await axios.get("/contacts");
+        return response.data;
+    } catch (e) {
+        return thunkAPI.rejectWithValue(e.message); 
+    }
+  
+});
+
+export const addContact = createAsyncThunk("contacts/addContact", async () => {
+    
+});
+
+export const deleteContact = createAsyncThunk("contacts/deleteContact", async () => {
+    
+})
